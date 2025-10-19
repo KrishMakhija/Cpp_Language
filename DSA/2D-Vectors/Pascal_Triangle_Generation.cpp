e6dced27706c3ebb2e2fd7750f921a8dfdc8110d
KrishMakhija <krishmakhija0603@gmail.com>
2025-10-19 20:21:47 +0530

    Implement Pascal's Triangle generation in C++

---
+++ DSA/2D-Vectors/Pascal_Triangle_Generation.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int numRows;
+    cout<<"Enter number of rows: ";
+    cin>>numRows;
+    vector< vector<int> > v;
+    for(int i=1; i<=numRows; i++){
+        vector<int> a(i);
+        v.push_back(a);
+    }
+
+    for(int i=0; i<numRows; i++){
+        for(int j=0; j<=i; j++){
+            if(j==0 || j==i){
+                v[i][j]=1;
+            }
+            else{
+                v[i][j]=v[i-1][j]+v[i-1][j-1];
+            }
+        }
+    }
+
+    for(int i=0; i<numRows; i++){
+        for(int j=0; j<=i; j++){
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
