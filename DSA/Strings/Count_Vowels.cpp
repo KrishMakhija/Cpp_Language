7f8dff05f1513399cd82750ec640d36811a62181
KrishMakhija <krishmakhija0603@gmail.com>
2025-10-20 12:42:30 +0530

    Create Count_Vowels.cpp

---
+++ DSA/Strings/Count_Vowels.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s;
+    int count=0;
+    getline(cin,s);
+
+    for(int i=0; s[i]!='\0'; i++){
+        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
+            count++;
+        }
+    }
+
+    cout<<count;
+}
