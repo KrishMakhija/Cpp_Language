d99fc0e959fa630a944b4610bda4cd434288c1cb
KrishMakhija <krishmakhija0603@gmail.com>
2025-11-07 11:30:33 +0530

    Create Grade_Of_Student.cpp

---
+++ OOPM/Grade_Of_Student.cpp
@@ -0,0 +1,48 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks;
+    cout<<"Enter the marks obtained bye the student: ";
+    cin>>marks;
+    
+    switch(marks/10){
+        case 10: 
+            cout<<"Grade is A+ "; 
+            break;
+        
+        case 9: 
+            cout<<"Grade is A";
+            break;
+        
+        case 8:
+            cout<<"Grade is B+";
+            break;
+            
+        case 7:
+            cout<<"Grade is C+";
+            break;
+            
+        case 6:
+            cout<<"Grade is C";
+            break;
+            
+        case 5:
+            cout<<"Grade is D+";
+            break;
+            
+        case 4:
+            cout<<"Grade is D";
+            break;
+            
+        case 3:
+            cout<<"Grade is D#";
+            break;
+            
+        default: 
+            cout<<"Grade is F";
+            break;
+    }
+    return 0;
+}
