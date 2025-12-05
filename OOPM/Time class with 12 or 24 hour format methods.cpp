bb70c05d3ebbe1784fef44b1a3ea84adbf1c0bb3
KrishMakhija <krishmakhija0603@gmail.com>
2025-12-05 12:32:59 +0530

    Implement Time class with 12/24 hour format methods

---
+++ OOPM/Time class with 12 or 24 hour format methods.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+class Time{
+    public:
+    int hours, minutes, seconds;
+    int i;
+    Time(){
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+        i=-1;
+    }
+    Time(int h, int m, int s, int a){
+        hours = h;
+        minutes = m;
+        seconds = s;
+        i = a;
+    }
+    
+    void setto24(){
+        if(i==1){
+            cout<<endl<<"Current time in 24 hour forat is:-"<<endl;
+            cout<<hours<<" : "<<minutes<<" : "<<seconds<<endl;
+        }
+        else if(i==2){
+            cout<<endl<<"Current time in 24 hour forat is:-"<<endl;
+            cout<<hours+12<<" : "<<minutes<<" : "<<seconds<<endl;
+        }
+        else{
+            cout<<endl<<"Current time in 24 hour forat is:-"<<endl;
+            cout<<hours<<" : "<<minutes<<" : "<<seconds<<endl;
+        }
+    }
+    
+    void setto12(){
+        if(hours>12){
+            i=2;
+            hours=hours-12;
+            cout<<endl<<"Current time in 24 hour forat is:-"<<endl;
+            cout<<hours<<" : "<<minutes<<" : "<<seconds<<" pm"<<endl;
+        }
+        else if(hours<12){
+            cout<<endl<<"Current time in 24 hour forat is:-"<<endl;
+            cout<<hours<<" : "<<minutes<<" : "<<seconds<<" am"<<endl;
+        }
+        else{
+            cout<<endl<<"Current time in 24 hour forat is:-"<<endl;
+            cout<<hours<<" : "<<minutes<<" : "<<seconds<<" pm"<<endl;
+        }
+    }
+};
+
+int main(){
+    int h, m, s, a;
+    cout<<"Ënter hours: ";
+    cin>>h;
+    cout<<"Ënter minutes: ";
+    cin>>m;
+    cout<<"Ënter seconds: ";
+    cin>>s;
+    cout<<"Ënter\n1. for am \n2. for pm \n-1. for 24 hour format"<<endl<<"Enter your choice: ";
+    cin>>a;
+    
+    Time t1;
+    Time t2(h, m, s, a);
+    t2.setto12();
+    t2.setto24();
+    
+}
